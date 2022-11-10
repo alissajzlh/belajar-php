df45bd0bf6f073a4b961dbf339bbcf45202aee64
Alisa Nuril Jazilah <alisanuril22@gmail.com>
2022-11-10 22:33:04 +0700

    initial init

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+echo"Hello World!<br>"; 
+$nama = "Ardhi";
+$umur = "21";
+echo"nama saya $nama, umur saya $umur tahun <br>";
+
+$nama1 = "Kurniawan";
+$umur1 = "17";
+echo"nama saya $nama1, umur saya $umur1 tahun <br>";
+
+$selisihumur = $umur - $umur1;
+
+echo "jadi selisih umur mereka $selisihumur tahun";
+?>
