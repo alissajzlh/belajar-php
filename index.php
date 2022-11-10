02c9fe885c2ada369f0054fe9882c05f7004b3e3
Alisa Nuril Jazilah <alisanuril22@gmail.com>
2022-11-10 22:36:10 +0700

    ubah isi

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 echo"Hello World!<br>"; 
-$nama = "Ardhi";
-$umur = "21";
+$nama = "Alisa";
+$umur = "20";
 echo"nama saya $nama, umur saya $umur tahun <br>";
 
-$nama1 = "Kurniawan";
-$umur1 = "17";
+$nama1 = "Nuril Jazilah";
+$umur1 = "21";
 echo"nama saya $nama1, umur saya $umur1 tahun <br>";
 
 $selisihumur = $umur - $umur1;
