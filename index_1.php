428acd54c3c8ffe8701025555d9fa0c9b452d7fc
Alisa Nuril Jazilah <alisanuril22@gmail.com>
2022-12-03 15:18:30 +0700

    praktik materi php_crud

---
+++ index_1.php
@@ -0,0 +1,14 @@
+<?php
+echo"Hello World!<br>"; 
+$nama = "Alisa";
+$umur = "20";
+echo"nama saya $nama, umur saya $umur tahun <br>";
+
+$nama1 = "Nuril Jazilah";
+$umur1 = "21";
+echo"nama saya $nama1, umur saya $umur1 tahun <br>";
+
+$selisihumur = $umur - $umur1;
+
+echo "jadi selisih umur mereka $selisihumur tahun";
+?>
